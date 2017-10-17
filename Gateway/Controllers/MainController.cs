c6588896a435c7f6522f6a52743dc479492aab6e
Иван Дружицкий <Shell952@outlook.com>
2017-10-17 21:20:20 +0300

    Added gateway tests

--- Gateway/Controllers/MainController.cs
+++ Gateway/Controllers/MainController.cs
@@ -37,13 +37,6 @@ namespace Gateway.Controllers
             this.newsService = newsService;
         }
 
-        // GET api/values
-        [HttpGet]
-        public async Task<IActionResult> Get()
-        {
-            return Ok();
-        }
-
         [HttpPost("login")]
         [AllowAnonymous]
         public async Task<IActionResult> Login(LoginModel loginModel)
@@ -51,15 +44,23 @@ namespace Gateway.Controllers
             var response = await accountsService.Login(loginModel);
             logger.LogInformation($"Response from accounts service: {response.StatusCode}");
 
-            if (response.StatusCode == System.Net.HttpStatusCode.OK)
+            if (response?.StatusCode == System.Net.HttpStatusCode.OK)
             {
                 logger.LogInformation("Adding claim");
                 response = await accountsService.AddClaim(loginModel.Username, await Authorize());
-                logger.LogInformation($"Adding claim response: {response.StatusCode}");
+                logger.LogInformation($"Adding claim response: {response?.StatusCode}");
                 return Ok();
             }
-            logger.LogWarning($"User {loginModel.Username} not authorized");
-            return Unauthorized();
+            else if (response != null)
+            {
+                logger.LogWarning($"User {loginModel.Username} not authorized");
+                return Unauthorized();
+            }
+            else
+            {
+                logger.LogCritical("Accounts service unavailable");
+                return NotFound("Service unavailable");
+            }
         }
 
 
@@ -70,15 +71,24 @@ namespace Gateway.Controllers
             var response = await accountsService.Register(userModel);
             logger.LogInformation($"Response from accounts service: {response.StatusCode}");
 
-            if (response.StatusCode == System.Net.HttpStatusCode.OK)
+            if (response?.StatusCode == System.Net.HttpStatusCode.OK)
             {
                 logger.LogInformation("Adding claim");
                 response = await accountsService.AddClaim(userModel.Username, await Authorize());
-                logger.LogInformation($"Adding claim response: {response.StatusCode}");
+                logger.LogInformation($"Adding claim response: {response?.StatusCode}");
                 return Ok();
             }
-            logger.LogError($"User {userModel.Username} not registered, error content: {await response.Content.ReadAsStringAsync()}");
-            return BadRequest(await response.Content.ReadAsStringAsync());
+            else if (response != null)
+            {
+                string respContent = response.Content != null ? await response.Content.ReadAsStringAsync() : string.Empty;
+                logger.LogError($"User {userModel.Username} not registered, error content: {respContent}");
+                return Unauthorized();
+            }
+            else
+            {
+                logger.LogCritical("Accounts service unavailable");
+                return NotFound("Service unavailable");
+            }
         }
 
         [HttpGet("logout")]
@@ -86,9 +96,17 @@ namespace Gateway.Controllers
         public async Task<IActionResult> Logout()
         {
             var response = await accountsService.RemoveClaim(await GetCurrentUsername());
-            logger.LogInformation($"Attempt to remove claim from users database, result: {response.StatusCode}");
             await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
-            return Ok();
+            if (response != null)
+            {
+                logger.LogInformation($"Attempt to remove claim from users database, result: {response.StatusCode}");
+                return Ok();
+            }
+            else
+            {
+                logger.LogCritical("Accounts service unavailable");
+                return NotFound("Service unavailable");
+            }
         }
 
         [HttpGet("news")]
@@ -97,25 +115,41 @@ namespace Gateway.Controllers
         {
             var name = await GetCurrentUsername();
             List<string> response = await newsService.GetNewsForUser(name, page, perpage);
-            logger.LogInformation($"Number of news for user {name}: {response.Count}");
-            return response;
+            if (response != null)
+            {
+                logger.LogInformation($"Number of news for user {name}: {response.Count}");
+                return response;
+            }
+            else
+            {
+                logger.LogCritical("News service unavailable");
+                return null;
+            }
         }
 
-        [HttpPost]
-        [Route("news")]
+        [HttpPost("news")]
+        [Authorize]
         public async Task<IActionResult> AddNews(NewsModel news)
         {
             news.Author = await GetCurrentUsername();
             var response = await newsService.AddNews(news);
-            logger.LogInformation($"Attempt to add news, response {response.StatusCode}");
-            if (response.IsSuccessStatusCode)
-                return Ok();
+            if (response != null)
+            {
+                logger.LogInformation($"Attempt to add news, response {response.StatusCode}");
+                if (response.IsSuccessStatusCode)
+                    return Ok();
+                else
+                    return BadRequest(response.Content?.ReadAsStringAsync()?.Result);
+            }
             else
-                return BadRequest(await response.Content.ReadAsStringAsync());
+            {
+                logger.LogCritical("News service unavailable");
+                return NotFound("Service unavailable");
+            }
         }
 
-        [HttpGet]
-        [Route("subscriptions")]
+        [HttpGet("subscriptions")]
+        [Authorize]
         public async Task<List<string>> GetSubscribedAuthors(int page = 0, int perpage = 0)
         {
             var user = await GetCurrentUsername();
@@ -125,32 +159,47 @@ namespace Gateway.Controllers
             return authors;
         }
 
-        [HttpPost]
-        [Route("subscriptions")]
+        [HttpPost("subscriptions")]
+        [Authorize]
         public async Task<IActionResult> AddSubscription(AddSubscriptionModel addSubscriptionModel)
         {
             var subscriber = await GetCurrentUsername();
             var response = await subscriptionsService.AddSubscription(subscriber, addSubscriptionModel.Author);
-            logger.LogInformation($"Attempt to add subscription {subscriber}-{addSubscriptionModel.Author}, response {response.StatusCode}");
-
-            if (response.IsSuccessStatusCode)
-                return Ok();
+            if (response != null)
+            {
+                logger.LogInformation($"Attempt to add subscription {subscriber}-{addSubscriptionModel.Author}, response {response.StatusCode}");
+                if (response.IsSuccessStatusCode)
+                    return Ok();
+                else
+                    return BadRequest(response.Content?.ReadAsStringAsync()?.Result);
+            }
             else
-                return BadRequest(await response.Content.ReadAsStringAsync());
+            {
+                logger.LogCritical("Subscriptions service unavailable");
+                return NotFound("Service unavailable");
+            }
         }
 
-        [HttpDelete]
-        [Route("subscriptions/{author}")]
+        [HttpDelete("subscriptions/{author}")]
+        [Authorize]
         public async Task<IActionResult> RemoveSubscription(string author)
         {
             var subscriber = await GetCurrentUsername();
             var response = await subscriptionsService.RemoveSubscription(subscriber, author);
-            logger.LogInformation($"Attempt to remove subscription {subscriber}-{author}, response {response.StatusCode}");
+            if (response != null)
+            {
+                logger.LogInformation($"Attempt to remove subscription {subscriber}-{author}, response {response.StatusCode}");
 
-            if (response.IsSuccessStatusCode)
-                return Ok();
+                if (response.IsSuccessStatusCode)
+                    return Ok();
+                else
+                    return BadRequest(response.Content?.ReadAsStringAsync()?.Result);
+            }
             else
-                return BadRequest(await response.Content.ReadAsStringAsync());
+            {
+                logger.LogCritical("Subscriptions service unavailable");
+                return NotFound("Service unavailable");
+            }
         }
 
         private async Task<string> Authorize()
@@ -159,13 +208,14 @@ namespace Gateway.Controllers
                 {
                     new Claim(ClaimsIdentity.DefaultNameClaimType, Guid.NewGuid().ToString())
                 }, "ApplicationCookie", ClaimsIdentity.DefaultNameClaimType, ClaimsIdentity.DefaultRoleClaimType);
-            await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(identity));
+            if (HttpContext != null)
+                await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(identity));
             return identity.Name;
         }
 
         private string GetCurrentCookie()
         {
-            string cookie = User.FindFirstValue(ClaimTypes.Name);
+            string cookie = User?.FindFirstValue(ClaimTypes.Name);
             logger.LogInformation($"Requested current cookie, value: {cookie}");
             return cookie;
         }
--- Tests/GatewayTests.cs
+++ Tests/GatewayTests.cs
@@ -1,13 +1,194 @@
+using Gateway.Controllers;
+using Gateway.Models;
+using Gateway.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Internal;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace Tests
 {
     [TestClass]
     public class GatewayTests
     {
-        [TestMethod]
-        public void TestMethod1()
+        private ILogger<MainController> logger;
+        private IAccountsService accountsService;
+        private ISubscriptionsService subscriptionsService;
+        private INewsService newsService;
+
+        [TestInitialize]
+        public void TestInitialize()
         {
+            var loggerMock = new Mock<ILogger<MainController>>();
+            logger = loggerMock.Object;
+            accountsService = GetAccountsService();
+            newsService = GetNewsService();
+            subscriptionsService = GetSubscriptionsService();
         }
+
+        [TestMethod]
+        public void TestLoginValid()
+        {
+            accountsService = GetAccountsService(loginCode: HttpStatusCode.OK);
+            var mainController = GetMainController();
+
+            var result = mainController.Login(Mock.Of<LoginModel>(lm => lm.Username == "User")).Result;
+            Assert.IsTrue(result is OkResult);
+        }
+
+        [TestMethod]
+        public void TestLoginNotValid()
+        {
+            accountsService = GetAccountsService(loginCode: HttpStatusCode.Unauthorized);
+            var mainController = GetMainController();
+
+            var result = mainController.Login(Mock.Of<LoginModel>(lm => lm.Username == "User")).Result;
+            Assert.IsTrue(result is UnauthorizedResult);
+        }
+
+        [TestMethod]
+        public void TestRegisterValid()
+        {
+            accountsService = GetAccountsService(registerCode: HttpStatusCode.OK);
+            var mainController = GetMainController();
+
+            var result = mainController.Register(Mock.Of<UserModel>(lm => lm.Username == "User")).Result;
+            Assert.IsTrue(result is OkResult);
+        }
+
+        [TestMethod]
+        public void TestRegisterNotValid()
+        {
+            accountsService = GetAccountsService(registerCode: HttpStatusCode.Unauthorized);
+            var mainController = GetMainController();
+
+            var result = mainController.Register(Mock.Of<UserModel>(lm => lm.Username == "User")).Result;
+            Assert.IsTrue(result is UnauthorizedResult);
+        }
+
+        [TestMethod]
+        public void TestGetNews()
+        {
+            var news = new List<string> { "news1", "news2" };
+            newsService = GetNewsService(news);
+            var mainController = GetMainController();
+
+            var result = mainController.GetNews().Result;
+            Assert.AreEqual(news, result);
+        }
+
+        [TestMethod]
+        public void TestAddNewsValid()
+        {
+            newsService = GetNewsService(addNewsCode: HttpStatusCode.OK);
+            var mainController = GetMainController();
+
+            var result = mainController.AddNews(Mock.Of<NewsModel>()).Result;
+            Assert.IsTrue(result is OkResult);
+        }
+
+        [TestMethod]
+        public void TestAddNewsNotValid()
+        {
+            newsService = GetNewsService(addNewsCode: HttpStatusCode.BadRequest);
+            var mainController = GetMainController();
+
+            var result = mainController.AddNews(Mock.Of<NewsModel>()).Result;
+            Assert.IsTrue(result is BadRequestObjectResult);
+        }
+
+        [TestMethod]
+        public void TestGetSubscribedAuthors()
+        {
+            var authors = new List<string> { "author1", "author2" };
+            subscriptionsService = GetSubscriptionsService(authors: authors);
+            var mainController = GetMainController();
+
+            var result = mainController.GetSubscribedAuthors().Result;
+            Assert.AreEqual(result, authors);
+        }
+
+        [TestMethod]
+        public void TestAddSubscriptionValid()
+        {
+            subscriptionsService = GetSubscriptionsService(addCode: HttpStatusCode.OK);
+            var mainController = GetMainController();
+
+            var result = mainController.AddSubscription(Mock.Of<AddSubscriptionModel>()).Result;
+            Assert.IsTrue(result is OkResult);
+        }
+
+        [TestMethod]
+        public void TestAddSubscriptionNotValid()
+        {
+            subscriptionsService = GetSubscriptionsService(addCode: HttpStatusCode.InternalServerError);
+            var mainController = GetMainController();
+
+            var result = mainController.AddSubscription(Mock.Of<AddSubscriptionModel>()).Result;
+            Assert.IsTrue(result is BadRequestObjectResult);
+        }
+
+        [TestMethod]
+        public void TestRemoveSubscriptionValid()
+        {
+            subscriptionsService = GetSubscriptionsService(removeCode: HttpStatusCode.OK);
+            var mainController = GetMainController();
+
+            var result = mainController.RemoveSubscription(string.Empty).Result;
+            Assert.IsTrue(result is OkResult);
+        }
+
+        [TestMethod]
+        public void TestRemoveSubscriptionNotValid()
+        {
+            subscriptionsService = GetSubscriptionsService(removeCode: HttpStatusCode.InternalServerError);
+            var mainController = GetMainController();
+
+            var result = mainController.RemoveSubscription(string.Empty).Result;
+            Assert.IsTrue(result is BadRequestObjectResult);
+        }
+
+        #region Support
+        private ISubscriptionsService GetSubscriptionsService(List<string> authors = null, HttpStatusCode addCode = HttpStatusCode.OK, HttpStatusCode removeCode = HttpStatusCode.OK)
+        {
+            return Mock.Of<ISubscriptionsService>(srv =>
+                srv.GetSubscribedAuthorsForName(It.IsAny<string>(), It.IsAny<int>(), It.IsAny<int>()) == Task.FromResult(authors) &&
+                srv.AddSubscription(It.IsAny<string>(), It.IsAny<string>()) == Task.FromResult(GetResponseMessage(addCode)) &&
+                srv.RemoveSubscription(It.IsAny<string>(), It.IsAny<string>()) == Task.FromResult(GetResponseMessage(removeCode)));
+        }
+
+        private INewsService GetNewsService(List<string> getNewsContent = null, HttpStatusCode addNewsCode = HttpStatusCode.OK)
+        {
+            return Mock.Of<INewsService>(srv =>
+                srv.GetNewsForUser(It.IsAny<string>(), It.IsAny<int>(), It.IsAny<int>()) == Task.FromResult(getNewsContent) &&
+                srv.AddNews(It.IsAny<NewsModel>()) == Task.FromResult(GetResponseMessage(addNewsCode)));
+        }
+
+        private MainController GetMainController()
+        {
+            return new MainController(logger, accountsService, subscriptionsService, newsService);
+        }
+
+        private IAccountsService GetAccountsService(HttpStatusCode loginCode = HttpStatusCode.OK,
+            HttpStatusCode registerCode = HttpStatusCode.OK,
+            string currentUsername = "User")
+        {
+            return Mock.Of<IAccountsService>(srv =>
+                srv.Login(It.IsAny<LoginModel>()) == Task.FromResult(GetResponseMessage(loginCode)) &&
+                srv.Register(It.IsAny<UserModel>()) == Task.FromResult(GetResponseMessage(registerCode)) &&
+                srv.GetNameByClaim(It.IsAny<string>()) == Task.FromResult(currentUsername));
+        }
+
+        private HttpResponseMessage GetResponseMessage(HttpStatusCode registerCode)
+        {
+            return Mock.Of<HttpResponseMessage>(hwr => hwr.StatusCode == registerCode);
+        }
+        #endregion
     }
 }
